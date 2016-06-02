252297942808ff97712d0b7b115fb14fa7753c5f
Alex Jacks and Charles Ewel <alex@>
2016-06-02 13:45:25 -0700

    create custom functions to handle 2 inputs in four possible branches

--- js/scripts.js
+++ js/scripts.js
@@ -18,65 +18,54 @@ var modulo = function(number1, number2){
 	return number1%number2;
 };
 
-$(document).ready(function(){
-	$("form#add").submit(function() {
-		var number1 = parseInt($("input#add1").val());
-		var number2 = parseInt($("input#add2").val());
-
-		$("#results").prepend("<h2>"+number1+"+"+number2+"="+add(number1,number2)+"</h2>");
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
 		event.preventDefault();
-	});
-
-	$("form#subtract").submit(function() {
-		var number1 = parseInt($("input#subtract1").val());
-		var number2 = parseInt($("input#subtract2").val());
-
-		$("#results").prepend("<h2>"+number1+"-"+number2+"="+subtract(number1,number2)+"</h2>");
-		event.preventDefault();
-
-	});
-
-	$("form#multiply").submit(function() {
-		var number1 = parseInt($("input#multiply1").val());
-		var number2 = parseInt($("input#multiply2").val());
-
-		$("#results").prepend("<h2>"+number1+"x"+number2+"="+multiply(number1,number2)+"</h2>");
-		event.preventDefault();
-	});
-
-	$("form#divide").submit(function() {
-		var number1 = parseInt($("input#divide1").val());
-		var number2 = parseInt($("input#divide2").val());
-
-		$("#results").prepend("<h2>"+number1+"/"+number2+"="+divide(number1,number2)+"</h2>");
-		event.preventDefault();
-	});
-
-	$("form#modulo").submit(function() {
-		var number1 = parseInt($("input#modulo1").val());
-		var number2 = parseInt($("input#modulo2").val());
-
-		$("#results").prepend("<h2>"+number1+"%"+number2+"="+modulo(number1,number2)+"</h2>");
-		event.preventDefault();
-	});
-
-	$("form#orderingaproduct").submit(function() {
-		var firstname = $("input#firstname").val();
-		var lastname = $("input#lastname").val();
-		var address= $("input#address").val();
-  	$("#receipt p").remove();
-		$("#receipt").prepend("<p> Thank you for ordering from our store. Your receipt is below</p><p>"+firstname+" "+lastname+"</p><p>"+address+"</p>");
-		event.preventDefault();
-	});
-
-	$("form#booking").submit(function() {
-		var name = $("input#name").val();
-		var date = $("input#date").val();
-		var starttime = $("input#starttime").val();
-		var endtime = $("input#endtime").val();
-
-		$("#confirmation").prepend("<h3>Event Details</h3><h4>"+name+"</h4><h5>"+date+"</h5><h5>"+starttime+" - "+endtime+"</h5>");
-
-		event.preventDefault();
-	});
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+		// console.log("1st number: " + number1);  // for debugging
+    // console.log("2nd number: " + number2);  // for debugging
+    // console.log("operator: " + operator);  // for debugging
+    var result;
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
+		
+    $("#output").text(result);
+  });
 });
+
+	// $("form#modulo").submit(function() {
+	// 	var number1 = parseInt($("input#modulo1").val());
+	// 	var number2 = parseInt($("input#modulo2").val());
+	//
+	// 	$("#results").prepend("<h2>"+number1+"%"+number2+"="+modulo(number1,number2)+"</h2>");
+	// 	event.preventDefault();
+	// });
+
+	// $("form#orderingaproduct").submit(function() {
+	// 	var firstname = $("input#firstname").val();
+	// 	var lastname = $("input#lastname").val();
+	// 	var address= $("input#address").val();
+  // 	$("#receipt p").remove();
+	// 	$("#receipt").prepend("<p> Thank you for ordering from our store. Your receipt is below</p><p>"+firstname+" "+lastname+"</p><p>"+address+"</p>");
+	// 	event.preventDefault();
+	// });
+	//
+	// $("form#booking").submit(function() {
+	// 	var name = $("input#name").val();
+	// 	var date = $("input#date").val();
+	// 	var starttime = $("input#starttime").val();
+	// 	var endtime = $("input#endtime").val();
+	//
+	// 	$("#confirmation").prepend("<h3>Event Details</h3><h4>"+name+"</h4><h5>"+date+"</h5><h5>"+starttime+" - "+endtime+"</h5>");
+	//
+	// 	event.preventDefault();
+	// });
